07e29fba84a65b5a970605d0c822b0882f5ae840
Anlon Zhu <zhu.anlon@gmail.com>
2023-10-24 18:11:41 -0400

    test_files

--- imagen_pytorch/test/__init__.py
+++ imagen_pytorch/test/__init__.py
@@ -1,1 +1,3 @@
 from imagen_pytorch.test import test_trainer
+from imagen_pytorch.test import test_video
+from imagen_pytorch.test import test_mnist
--- imagen_pytorch/test/test_trainer.py
+++ imagen_pytorch/test/test_trainer.py
@@ -4,14 +4,15 @@ from imagen_pytorch.t5 import t5_encode_text
 from torch.utils.data import Dataset
 import torch
 
+
 def test_trainer_instantiation():
     unet1 = dict(
-        dim = 8,
-        dim_mults = (1, 1, 1, 1),
-        num_resnet_blocks = 1,
-        layer_attns = False,
-        layer_cross_attns = False,
-        attn_heads = 2
+        dim=8,
+        dim_mults=(1, 1, 1, 1),
+        num_resnet_blocks=1,
+        layer_attns=False,
+        layer_cross_attns=False,
+        attn_heads=2
     )
 
     imagen = ImagenConfig(
@@ -23,21 +24,24 @@ def test_trainer_instantiation():
         imagen=imagen
     )
 
+
 def test_trainer_step():
     class TestDataset(Dataset):
         def __init__(self):
             super().__init__()
+
         def __len__(self):
             return 16
+
         def __getitem__(self, index):
             return (torch.zeros(3, 64, 64), torch.zeros(6, 768))
     unet1 = dict(
-        dim = 8,
-        dim_mults = (1, 1, 1, 1),
-        num_resnet_blocks = 1,
-        layer_attns = False,
-        layer_cross_attns = False,
-        attn_heads = 2
+        dim=8,
+        dim_mults=(1, 1, 1, 1),
+        num_resnet_blocks=1,
+        layer_attns=False,
+        layer_cross_attns=False,
+        attn_heads=2
     )
 
     imagen = ImagenConfig(
